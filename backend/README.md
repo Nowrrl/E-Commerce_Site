fb310205fb8e7bae39d452e4a73bdacfb95ac79c
MuradAsadzade <muradesedzade2004@gmail.com>
2025-03-04 17:11:54 +0400

    frontend and backend folders seperated

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+# CS308Project
+
+
+very nice project :)
---
+++ backend/src/main/java/org/example/cs308project/loginregister/model/register_model.java
@@ -0,0 +1,53 @@
+package org.example.cs308project.loginregister.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "register_table")
+public class register_model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
